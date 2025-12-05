eaf5f75c7837c11a697a553e511b7f4594b125f8
bruno moraes moraes <brunomoraesm072@gmail.com>
2025-12-05 18:19:06 +0000

    File changes

--- src/pages/ComprovantesInternos.jsx
+++ src/pages/ComprovantesInternos.jsx
@@ -831,13 +831,7 @@ export default function ComprovantesInternos() {
                                 </div>
                             )}
 
-                            {/* Indicador de processamento IA */}
-                            {processandoIA && (
-                                <div className="flex items-center gap-2 p-3 bg-sky-50 rounded-xl text-sky-700">
-                                    <Loader2 className="w-5 h-5 animate-spin" />
-                                    <span className="text-sm">Lendo informações da imagem com IA...</span>
-                                </div>
-                            )}
+
                         </div>
 
                         {/* Observações */}
